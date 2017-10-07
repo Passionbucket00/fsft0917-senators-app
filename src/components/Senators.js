1a67b89962459f70a6051d1dc42368887c625d8d
Greeneink <jeffrey.greene.mp3@gmail.com>
2017-10-07 10:29:35 -0600

    commit.2

---
+++ src/components/Senators.js
@@ -0,0 +1,14 @@
+import REact, { Component } from 'react';
+
+import senatorData from '../senatorData';
+
+export default class Senators extends Component {
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
