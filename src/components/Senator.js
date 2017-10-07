3daf6e169c28151ed88e96610422680ddeb767b3
Greeneink <jeffrey.greene.mp3@gmail.com>
2017-10-07 11:16:04 -0600

    commit

--- src/components/Senator.js
+++ src/components/Senator.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 export default class Senator extends Component {
 
     render() {
+
+        const senator = this.props.senator;
+
         return (
             <div>
-
+                {senator.name} {senator.description}
             </div>
         )
     }
--- src/components/Senators.js
+++ src/components/Senators.js
@@ -1,14 +1,44 @@
-import REact, { Component } from 'react';
+import React, { Component } from 'react';
 
 import senatorData from '../senatorData';
+import './Senator.css';
+import Senator from './Senator';
 
 export default class Senators extends Component {
 
+    filterByParty(party) {
+            return senatorData.filter(senator => senator.party === party)
+        }
+    
+
+        renderRepublicans() {
+            return this.filterByParty('Republican').map(senator => {
+                return <Senator  senator={senator}/>
+            });
+        }
+
     render() {
+
+        const republicans = this.renderRepublicans();
         return (
             <div>
-            
-            </div>
+                <form>
+                    <input type="search" placeholder="Search" />
+                </form>
+                <div className="senator-columns">
+                    <div className="senator-republicans">
+                        <h1>Republicans</h1>
+                            <div>{republicans}
+                            </div>
+                    </div>
+                    <div className="senator-democrats">
+                        <h1>Democrats</h1>
+                            <div>
+                            </div>
+                    </div>
+
+                    </div>
+                </div>
         )
     }
 }
